439865ce98277ed4e176634ff76fabc824f6c533
Md-Aashiqq <md.aashiq.tvl@gmail.com>
2021-02-28 21:49:42 +0530

    something works

--- index.mjs
+++ index.mjs
@@ -2,15 +2,25 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import path from "path";
+
+// Import a fetch implementation for Node.js
+import fetch from "node-fetch";
+
 import * as canvas from "canvas";
 
 import * as faceapi from "face-api.js";
 
-import * as tf from "@tensorflow/tfjs-node";
+// import * as tf from "@tensorflow/tfjs-node";
+
+// Make face-api.js use that fetch implementation
+// faceapi.env.monkeyPatch({ fetch: fetch });
 
 const app = express();
 
 app.use(express.static("public"));
+
+// const MODELS_URL = path.join(__dirname, "./models");
 
 // const loadModel = async () => {
 //   // Promise.all([
@@ -27,11 +37,18 @@ app.use(express.static("public"));
 // };
 
 app.get("/", async (req, res) => {
-  // loadModel();
+  const MODEL_URL = `./public/models/`;
 
-  const data = await faceapi.nets.faceRecognitionNet.loadFromUri("/models");
-
-  res.send(data);
+  faceapi.default.nets.ssdMobilenetv1
+    .loadFromDisk(MODEL_URL)
+    .then(faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_URL))
+    .then(faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_URL))
+    .then(faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_URL))
+    .then(() => console.log("model loaed sucessfully"))
+    .catch((error) => {
+      console.log(error);
+    });
+  res.send({ data: "hello" });
 });
 
 const port = process.env.PORT || 3000;
