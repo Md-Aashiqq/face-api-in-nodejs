54e61604c76e50deb8cf5980c0e614d38d687531
Md-Aashiqq <md.aashiq.tvl@gmail.com>
2021-02-28 19:59:52 +0530

    changes in api

--- index.mjs
+++ index.mjs
@@ -2,13 +2,29 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
+import * as canvas from 'canvas';
+
+import * as faceapi from 'face-api.js';
 
 import * as tf from "@tensorflow/tfjs-node";
 
 const app = express();
 
+app.use(express.static(__dirname + "/public"));
+
+
+
 app.get("/", (req, res) => {
-  res.send({ msg: "hello", td: tf });
+  Promise.all([
+    faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+    faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+  ]).then(() => {
+    console.log('model loded sucessfully')
+  });
+
+  res.send('hello world');
+
 });
 
 const port = process.env.PORT || 3000;
